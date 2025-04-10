2719c0210101cf98769792f0ca52a6667c3e9878
Marek Gwozdz <marek.gwozdz@rockwellautomation.com>
2025-04-10 08:58:06 +0200

    types and standard test

--- reproducer.ts
+++ reproducer.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { expectFalseCustom } from './myHelpers.ts';
+import { expectFalseCustom } from './myHelpers';
+
 describe('SONAR assertion', function() {
   // function expectFalseCustom(value) {
   //   expect(value).to.be.false;
@@ -8,5 +9,9 @@ describe('SONAR assertion', function() {
     // when below expectFalseCustom method is in this file with test (uncomment above) then the sonar is no raise a problem
     // when below expectFalseCustom method is imported from other file then the sonar raise problem,
     expectFalseCustom(false);
+  });
+
+  it('Standard test', function() {
+    expect(false).to.be.false
   });
 });
